1d29f9450c19681a9903a24c7a67ba5f98a79ac9
Sylvester Keil <sylvester@keil.or.at>
2015-01-26 13:01:10 +0100

    test client options initialization

--- test/client.js
+++ test/client.js
@@ -11,14 +11,33 @@ chai.use(require('sinon-chai'));
 chai.use(require('chai-as-promised'));
 
 var HydraClient = require('../lib/client');
+var HTTPClient  = require('../lib/http');
 
 //var Session      = require('arkivo/lib/sync').Session;
 //var Subscription = arkivo.Subscription;
 
 
 describe('HydraClient', function () {
+  var client;
+
+  beforeEach(function () { client = new HydraClient(); });
 
   it('is a constructor function', function () {
     expect(HydraClient).to.be.a('function');
+    expect(client).to.be.instanceOf(HydraClient);
+  });
+
+  it('inherits from HTTPClient', function () {
+    expect(client).to.be.instanceOf(HTTPClient);
+  });
+
+  describe('#options', function () {
+    it('has CRUD paths', function () {
+      expect(client)
+        .to.have.property('options')
+        .that.has.keys(['create', 'update', 'delete']);
+
+      expect(client.options.update).to.match(/api\/items\/:id$/);
+    });
   });
 });
