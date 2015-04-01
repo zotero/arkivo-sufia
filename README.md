eca3582a4826efaddee587bf1adaa2ba195bb993
Sylvester Keil <sylvester@keil.or.at>
2015-04-01 11:48:47 +0200

    rename hydra to sufia

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# arkivo-hydra
+# arkivo-sufia
 
-[![Build Status](https://travis-ci.org/inukshuk/arkivo-hydra.svg?branch=master)](https://travis-ci.org/inukshuk/arkivo-hydra)
-[![Coverage Status](https://coveralls.io/repos/inukshuk/arkivo-hydra/badge.svg)](https://coveralls.io/r/inukshuk/arkivo-hydra)
+[![Build Status](https://travis-ci.org/inukshuk/arkivo-sufia.svg?branch=master)](https://travis-ci.org/inukshuk/arkivo-hydra)
+[![Coverage Status](https://coveralls.io/repos/inukshuk/arkivo-sufia/badge.svg)](https://coveralls.io/r/inukshuk/arkivo-hydra)
 
 A Zotero/Arkivo plugin to connect to Project Hydra services
--- test/plugin.js
+++ test/plugin.js
@@ -11,7 +11,7 @@ chai.use(require('sinon-chai'));
 chai.use(require('chai-as-promised'));
 
 //var arkivo       = require('arkivo');
-var hydra        = require('../lib/plugin');
+var sufia        = require('../lib/plugin');
 
 var Session      = require('arkivo/lib/sync').Session;
 
@@ -22,32 +22,32 @@ var plugins      = require('arkivo/lib/plugins');
 
 describe('Hydra Plugin', function () {
 
-  before(function () { plugins.add(hydra); });
+  before(function () { plugins.add(sufia); });
   after(function () { plugins.reset(); });
 
   it('is available', function () {
-    expect(plugins.available.hydra).to.be.an('object');
+    expect(plugins.available.sufia).to.be.an('object');
   });
 
   it('can be used', function () {
-    expect(plugins.use('hydra')).to.be.instanceof(plugins.Plugin);
+    expect(plugins.use('sufia')).to.be.instanceof(plugins.Plugin);
   });
 
   it('is configurable', function () {
-    expect(plugins.use('hydra').configurable).to.be.true;
+    expect(plugins.use('sufia').configurable).to.be.true;
   });
 
   it('has a summary', function () {
-    expect(plugins.use('hydra')).to.have.property('summary');
+    expect(plugins.use('sufia')).to.have.property('summary');
   });
 
   it('must be configured with a host', function () {
     var config = {};
 
-    expect(plugins.use.bind(plugins, 'hydra', config)).to.throw(Error);
+    expect(plugins.use.bind(plugins, 'sufia', config)).to.throw(Error);
 
     config.host = 'http://example.com:8181';
 
-    expect(plugins.use.bind(plugins, 'hydra', config)).to.not.throw(Error);
+    expect(plugins.use.bind(plugins, 'sufia', config)).to.not.throw(Error);
   });
 });
